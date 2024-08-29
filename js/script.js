aea3328f8703f7bfcf132e0995b591b460dca6da
Agus Asso <agusasso22@gmail.com>
2024-08-29 19:59:25 +0900

    tambah file

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// single line comment
+
+/*
+multiple line comment
+bisa tulis juga disini
+*/
+
+var namaDepan = "Agus";
+var namaBelakang = "Asso";
+
+document.getElementById("nama").innerHTML = namaDepan + namaBelakang;
